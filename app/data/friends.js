7e140d46993899e84e4ee7c4b46fad320bf5056f
Matthew Bayers <mbayers6370@users.noreply.github.com>
2020-03-05 01:50:49 -0800

    original commit

---
+++ app/data/friends.js
@@ -0,0 +1,69 @@
+var friends = [ 
+    {
+    "name":"James",
+    "photo": "../public/assets/images/dandelion.jpg",
+    "scores":[
+        5,
+        1,
+        4,
+        4,
+        5,
+        1,
+        2,
+        5,
+        4,
+        1
+      ]
+   },
+   {
+    "name":"Jane",
+    "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+    "scores":[
+        5,
+        4,
+        3,
+        5,
+        3,
+        2,
+        1,
+        5,
+        4,
+        3
+      ]
+   },
+   {
+    "name":"Lana",
+    "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+    "scores":[
+        1,
+        1,
+        2,
+        2,
+        3,
+        3,
+        1,
+        3,
+        4,
+        5
+      ]
+   },
+   {
+    "name":"Carey",
+    "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+    "scores":[
+        4,
+        3,
+        3,
+        3,
+        2,
+        1,
+        1,
+        1,
+        2,
+        1
+      ]
+   }
+
+];
+
+module.exports = friends;
